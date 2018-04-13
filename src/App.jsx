bfde15f5d1d49ce16499b0da3a3dfc4dfa45a222
Deepak Mahakale <deepakmahakale@gmail.com>
2018-04-13 20:32:12 +0530

    Change extension

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import Item from './Item';
+import Item from './Item.jsx';
 import './App.css';
 
 class App extends Component {
@@ -10,37 +10,8 @@ class App extends Component {
     this.state = { todo_list: [] }
   }
 
-  static getDerivedStateFromProps(nextProps, prevState) {
-    console.log('parent: getDerivedStateFromProps');
-    return null;
-  }
-
-  componentDidMount() {
-    console.log('parent: componentDidMount');
-    // API calls
-  }
-
-  shouldComponentUpdate(nextProps, nextState) {
-    console.log('parent: shouldComponentUpdate');
-    return true;
-  }
-
-  getSnapshotBeforeUpdate(prevProps, prevState) {
-    console.log('parent: getSnapshotBeforeUpdate');
-    return null;
-  }
-
-  componentDidUpdate(prevProps, prevState, snapshot) {
-    console.log('parent: componentDidUpdate');
-  }
-
-  componentWillUnmount() {
-    console.log('parent: componentWillUnmount');
-  }
-
-  // componentDidCatch(error, info) {}
-
-  addItemToList() {
+  addItemToList(e) {
+    e.preventDefault();
     let list = [...this.state.todo_list]
     if(this.inputField.current.value !== '') {
       list.push(this.inputField.current.value);
@@ -57,9 +28,9 @@ class App extends Component {
       <div className="App">
         <h1>To Do List</h1>
         <p>(Add new ToDo items to the list using the input box)</p>
-        <form action="#">
+        <form onSubmit={this.addItemToList}>
           <input type="text" ref={ this.inputField }/>
-          <button type="submit" onClick={ this.addItemToList }>Add</button>
+          <button type="submit" >Add</button>
         </form>
         <ol className="Todo">
           {
